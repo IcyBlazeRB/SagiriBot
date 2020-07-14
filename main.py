55c2aa00c2502e365b5d5e0cf5b33b516e213c43
IcyBlazeRB <44699018+IcyBlazeRB@users.noreply.github.com>
2020-07-15 00:33:39 +1200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def get_prefix(client, message):
         
         return prefixes[str(message.guild.id)]
 
-client = commands.Bot(command_prefix=get_prefix)
+client = commands.Bot(command_prefix=get_prefix and commands.when_mentioned_or("owo ")) 
 client.remove_command("help")
 
 @client.event
@@ -22,7 +22,7 @@ async def on_guild_join(guild):
         with open("prefixes.json","r") as f:
                 prefixes = json.load(f)
         
-        prefixes[str(guild.id)] = "owo "
+        prefixes[str(guild.id)] = "uwu "
 
         with open("prefixes.json","w") as f:
                 json.dump(prefixes, f, indent=4)
@@ -46,13 +46,26 @@ async def reload(ctx, extension):
         else:
                 await ctx.send("Unable to do that, as you're not the bot owner!")
 
+@client.command()
+async def load(ctx, extension):
+        if ctx.message.author.id == 261249219812261888:
+                client.load_extension(f'cogs.{extension}')
+                await ctx.send("Cog Loaded!")
+        else:
+                await ctx.send("Unable to do that, as you're not the bot owner!")
+
+@client.command()
+async def unload(ctx, extension):
+        if ctx.message.author.id == 261249219812261888:
+                client.unload_extension(f'cogs.{extension}')
+                await ctx.send("Cog Unloaded!")
+        else:
+                await ctx.send("Unable to do that, as you're not the bot owner!")
+
 
 for filename in os.listdir('./cogs'):
         if filename.endswith('.py'):
                 client.load_extension(f'cogs.{filename[:-3]}')
 
+client.run("NzI5OTQyNzYzNzM0NTY0OTE0.XwQSSg.TgNgVt837qnA-MpKBoxAF0URZFk")
 
-
-
-
-client.run(os.getenv("TOKEN"))
