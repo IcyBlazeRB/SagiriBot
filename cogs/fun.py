43b16a862d67b33ab7ef10883e0927506b488719
IcyBlazeRB <44699018+IcyBlazeRB@users.noreply.github.com>
2020-10-14 19:11:54 +1300

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import random
+import requests
+import os
 
 class fun(commands.Cog):
 
@@ -52,14 +54,36 @@ class fun(commands.Cog):
                         await ctx.send("I rolled a " + str(die))
 
         @commands.command()
-        async def flip(self,ctx):
-                options = ["The coin landed on Heads!","The coin landed on Tails!","Wow, whats the chances? The coin landed it on its side!"]
-                distirbution = [0.495, 0.495, 0.01]
-                choice = str(random.choices(options, distirbution))
-                schoice = choice[2:-2:]
-                embed=discord.Embed(title="Results of the coinflip:", description=schoice, color=0xa700bd)
-                await ctx.send(embed=embed)
+        async def die(self, ctx, *num):
+                if num:
+                        if num.isnumeric():
+                                die = random.randint(1,int(num))
+                                await ctx.send("I rolled a " + str(die))
+                        else:
+                                await ctx.send("Failed to provide a valid argument!")
 
+                elif not num:
+                        die = random.randint(1,6)
+                        await ctx.send("I rolled a " + str(die))
+
+        @commands.command(aliases=['neko'])
+        async def cat(self,ctx):
+                url = "https://cataas.com/cat"
+                r = requests.get(url)
+                filename = "cat.jpg"
+                with open(filename,'wb') as f:
+                        f.write(r.content)
+                
+                embed=discord.Embed(title="Heres your cat! 🐱")
+                embed.set_image(
+                url="attachment://cat.jpg"
+                )
+                image = discord.File("cat.jpg")
+                await ctx.send(
+                        embed=embed,
+                        file=image
+                )
+                os.remove("cat.jpg")
 
 
 
