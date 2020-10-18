aaa48a638acb6875c1c8f2a2f6d6a4fb65998026
IcyBlazeRB <44699018+IcyBlazeRB@users.noreply.github.com>
2020-10-19 08:35:14 +1300

    Update useful.py

--- cogs/useful.py
+++ cogs/useful.py
@@ -5,24 +5,9 @@ import os
 import time
 import json
 import random
-import ast
 from gtts import gTTS
 
 
-def insert_returns(body):
-    # insert return stmt if the last expression is a expression statement
-    if isinstance(body[-1], ast.Expr):
-        body[-1] = ast.Return(body[-1].value)
-        ast.fix_missing_locations(body[-1])
-
-    # for if statements, we insert returns into the body and the orelse
-    if isinstance(body[-1], ast.If):
-        insert_returns(body[-1].body)
-        insert_returns(body[-1].orelse)
-
-    # for with blocks, again we insert returns into the body
-    if isinstance(body[-1], ast.With):
-        insert_returns(body[-1].body)
 
 
 
